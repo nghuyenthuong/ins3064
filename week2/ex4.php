cb274e185ad63d45bbe4e229a477327d56069230
nghuyenthuong <nguyenhuyenthuong2707@gmail.com>
2025-09-25 11:08:47 +0700

    Update ex4.php

--- week2/ex4.php
+++ week2/ex4.php
@@ -11,7 +11,7 @@
     $x=$_GET['x'];
     $y=$_GET['y'];
     
-    echo "x+y:".( $x+$y)."<br/>";
+    echo "$x + $y:".( $x+$y)."<br/>";
     echo "x-y:".( $x-$y)."<br/>";
     echo "x /  y:".( $x/$y)."<br/>";
     echo "x * y:".( $x * $y)."<br/>";
