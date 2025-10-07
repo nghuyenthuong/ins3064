bf43ab38198b93e66cc2cc0df9b7f846bb589588
nghuyenthuong <nguyenhuyenthuong2707@gmail.com>
2025-10-07 18:27:57 +0700

    week4

--- week4/regester.php
+++ week4/regester.php
@@ -1,14 +1,40 @@
- tạo trang regester sử sụng database login_demo , bảng users có các trường id, username, password
 <?php
 include("db_connect.php");
+ 
 if (isset($_POST['register'])) {
     $username = $_POST['username'];
     $password = md5($_POST['password']);
-
-    $query = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
-    if (mysqli_query($link, $query)) {
-        echo "Registration successful!";
+ 
+    $check = mysqli_query($link, "SELECT * FROM users WHERE username='$username'");
+    if (mysqli_num_rows($check) > 0) {
+        echo "Existed";
     } else {
-        echo "Error: " . mysqli_error($link);
+        $query = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+        if (mysqli_query($link, $query)) {
+            echo "Successful";
+        } else {
+            echo "Error " . mysqli_error($link);
+        }
     }
 }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+</head>
+<body>
+    <h2>Register</h2>
+    <form action="" method="post">
+        <label>Username:</label>
+        <input type="text" name="username" required><br>
+
+        <label>Password:</label>
+        <input type="password" name="password" required><br>
+
+        <input type="submit" name="register" value="Register">
+    </form>
+</body>
+</html>
